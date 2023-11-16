664b71ab209a734e1177ea9474141b4fb356afbb
fairygarbage <111027159+fairygarbage@users.noreply.github.com>
2023-11-16 13:15:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 from PIL import Image
-from PIL import Image, ImageFilter
 import streamlit as st
 
 modelo_ruta = 'modelo_deteccion_edad.h5'
 model = load_model(modelo_ruta)
 
-gender_dict = {0:'Male', 1:'Female'}
+gender_dict = {0: 'Male', 1: 'Female'}
 
 st.title("Age Detection")
 
@@ -21,16 +20,15 @@ def extract_feature(image):
     img_array = img_array / 255.0  # Normalizar valores al rango [0, 1]
     return img_array
 
-
 def age_range(edad_predicha):
-    rango_inferior = edad_predicha - 2
+    rango_inferior = max(0, edad_predicha - 2)
     rango_superior = edad_predicha + 2
-    print(f"Edad entre {rango_inferior} y {rango_superior} años.")
+    st.info(f"Edad entre {rango_inferior} y {rango_superior} años.")
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
     sujeto = extract_feature(img)
-    sujeto = sujeto.reshape((1, 128, 128, 1))  
+    sujeto = sujeto.reshape((1, 128, 128, 1))  # Agregar dimensión para el modelo
     pred = model.predict(sujeto)
     pred_gender = gender_dict[round(pred[0][0])]
     pred_age = round(pred[0][1])
